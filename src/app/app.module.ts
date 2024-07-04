e648fcfa85b20e90b82ae0b23c194980b1c7bc95
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-04 07:33:40 +0530

    added ui for dashboad. added logo but other images and components are yet to be added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,46 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     DashboardComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [
+//     provideClientHydration()
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+//---------------new----------------
+
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // Add BrowserAnimationsModule here
+    MatIconModule, // Add MatIconModule here
     AppRoutingModule
   ],
   providers: [
