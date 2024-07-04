4c3964943258adb2f4f525606f75a5f313d3c19d
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-04 16:03:29 +0530

    added logo, profile pic, notification, added center, added colors to last login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     DashboardComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [
-//     provideClientHydration()
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-//---------------new----------------
-
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
@@ -31,11 +6,13 @@ import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
